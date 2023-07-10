4f5784fdea995b963765a4e6f69714aba205396a
spengrah <code@spengrah.xyz>
2023-07-10 13:17:24 -0500

    update to new Hats.sol v1 address

--- script/HatsModuleFactory.s.sol
+++ script/HatsModuleFactory.s.sol
@@ -6,11 +6,11 @@ import { HatsModule, HatsModuleFactory, IHats, deployModuleFactory } from "src/u
 
 contract Deploy is Script {
   HatsModuleFactory public factory;
-  IHats public constant hats = IHats(0x9D2dfd6066d5935267291718E8AA16C8Ab729E9d); // v1.hatsprotocol.eth
+  IHats public constant hats = IHats(0x3bc1A0Ad72417f2d411118085256fC53CBdDd137); // v1.hatsprotocol.eth
   bytes32 public SALT = bytes32(abi.encode(0x4a75)); // ~ H(4) A(a) T(7) S(5)
 
   // default values
-  string public version = "0.1.0"; // increment with each deploy
+  string public version = "0.1.1"; // increment with each deploy
   bool private verbose = true;
 
   /// @notice Override default values, if desired
